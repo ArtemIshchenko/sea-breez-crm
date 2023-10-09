be3447c9820cc02fc59ff6c4b2307de91ed58c74
Artem <artem.ishchenko@skynix.co>
2023-10-09 09:31:14 +0300

    update project

--- mail/specificationSet-text.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this yii\web\View */
-/* @var $project */
-
-$url = Yii::$app->urlManager->createAbsoluteUrl([Yii::$app->params['customerAppUrl'], '#' => '/projects/' . $project->id]);
-?>
-Здравствуйте!
-К вашему проекту "<?= $project->title ?>" была подана спецификация.
-<?= $url ?>.
